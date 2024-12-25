f7ed0bac30d6285fdd33154d5c0116cfebdcbcfc
Chris Carvache <chris@thewaterproject.org>
2024-12-25 10:39:54 -0500

    Testing route config

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// More stuff to version control
+// And then there's even more stuff to version control
